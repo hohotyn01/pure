20da43f0c46038bd8922712ff4aff11ad6b522e3
hohotyn01 <hohotyn.success@gmail.com>
2019-08-20 18:25:19 +0300

    The first commit

---
+++ laravel/resources/views/extras.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.site')
+
+@section('header')
+    @include('site.header')
+@endsection
+
+@section('content')
+    <strong>Now you choice some extras to finish up</strong>
+    <div class="container">
+        <form action="" method="post">
+
+        </form>
+    </div>
+
+
+@endsection
---
+++ laravel/resources/views/your_home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.site')
+
+@section('header')
+    @include('site.header')
+@endsection
+
+@section('content')
+    <strong>Now we need information your home</strong>
+    <div class="container">
+        <form action="" method="post">
+
+        </form>
+    </div>
+
+
+@endsection
